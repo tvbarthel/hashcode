4a19319db15f7d8c49fdabaf01490dc5fd52cfda
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2017-02-23 18:47:28 +0100

    [Qualification] Upload data and subject

---
+++ src/main/java/Qualification.java
@@ -0,0 +1,20 @@
+/**
+ * Qualification Round 2017
+ */
+public class Qualification {
+    private static final String KITTEN_IN = "files/qualification_2017/kittens.in";
+    private static final String KITTEN_OUT = "files/qualification_2017/kittens.out";
+
+    private static final String ZOO_IN = "files/qualification_2017/me_at_the_zoo.in";
+    private static final String ZOO_OUT = "files/qualification_2017/me_at_the_zoo.out";
+
+    private static final String TRENDING_IN = "files/qualification_2017/trending_today.in";
+    private static final String TRENDING_OUT = "files/qualification_2017/trending_today.out";
+
+    private static final String WORTH_SPREADING_IN = "files/qualification_2017/videos_worth_spreading.in";
+    private static final String WORTH_SPREADING_OUT = "files/qualification_2017/videos_worth_spreading.out";
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the qualification round!");
+    }
+}
