f55972e784f468a2b5353d69795d42dd008ef4b4
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2017-02-23 19:37:09 +0100

    [Qualification] Parse the problem

--- src/main/java/Qualification.java
+++ src/main/java/Qualification.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -5,6 +6,9 @@ import java.util.List;
  * Qualification Round 2017
  */
 public class Qualification {
+    private static final String EXAMPLE_IN = "files/qualification_2017/example.in";
+    private static final String EXAMPLE_OUT = "files/qualification_2017/example.out";
+
     private static final String KITTEN_IN = "files/qualification_2017/kittens.in";
     private static final String KITTEN_OUT = "files/qualification_2017/kittens.out";
 
@@ -17,9 +21,105 @@ public class Qualification {
     private static final String WORTH_SPREADING_IN = "files/qualification_2017/videos_worth_spreading.in";
     private static final String WORTH_SPREADING_OUT = "files/qualification_2017/videos_worth_spreading.out";
 
+
     public static void main(String[] args) {
         System.out.println("Welcome to the qualification round!");
+
+        Problem problem = parseProblem(EXAMPLE_IN);
+
+        System.out.println("Bye bye to the qualification round!");
     }
+
+    public static Problem parseProblem(String fileName) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line;
+            line = br.readLine();
+
+            String[] firstLineParams = line.split(" ");
+            int numberOfVideos = Integer.parseInt(firstLineParams[0]);
+            int numberOfEndPoints = Integer.parseInt(firstLineParams[1]);
+            int numberOfRequests = Integer.parseInt(firstLineParams[2]);
+            int numberOfCaches = Integer.parseInt(firstLineParams[3]);
+            int cacheCapacity = Integer.parseInt(firstLineParams[4]);
+
+            ArrayList<Cache> caches = new ArrayList<Cache>(numberOfCaches);
+            for (int i = 0; i < numberOfCaches; i++) {
+                Cache cache = new Cache(i, cacheCapacity);
+                caches.add(cache);
+            }
+
+            ArrayList<Video> videos = new ArrayList<Video>(numberOfVideos);
+            line = br.readLine();
+            String[] videosSize = line.split(" ");
+            for (int i = 0; i < numberOfVideos; i++) {
+                int videoSize = Integer.parseInt(videosSize[i]);
+                Video video = new Video(i, videoSize);
+                videos.add(video);
+            }
+
+            ArrayList<EndPoint> endPoints = new ArrayList<EndPoint>(numberOfEndPoints);
+            for (int i = 0; i < numberOfEndPoints; i++) {
+                line = br.readLine();
+                String[] endPointParams = line.split(" ");
+                int latencyFromDataCenter = Integer.parseInt(endPointParams[0]);
+                int numberOfConnectedCache = Integer.parseInt(endPointParams[1]);
+                EndPoint endPoint = new EndPoint(i, latencyFromDataCenter);
+                for (int j = 0; j < numberOfConnectedCache; j++) {
+                    line = br.readLine();
+                    String[] latencyParams = line.split(" ");
+                    int cacheId = Integer.parseInt(latencyParams[0]);
+                    int latencyValue = Integer.parseInt(latencyParams[1]);
+                    Cache cache = caches.get(cacheId);
+
+                    Latency latency = new Latency(cache, latencyValue);
+                    endPoint.latencies.add(latency);
+                }
+                endPoints.add(endPoint);
+            }
+
+            ArrayList<Request> requests = new ArrayList<Request>(numberOfRequests);
+            for (int i = 0; i < numberOfRequests; i++) {
+                line = br.readLine();
+                String[] requestParams = line.split(" ");
+                int videoId = Integer.parseInt(requestParams[0]);
+                int endPointId = Integer.parseInt(requestParams[1]);
+                int number = Integer.parseInt(requestParams[2]);
+
+                Video video = videos.get(videoId);
+                EndPoint endPoint = endPoints.get(endPointId);
+
+                Request request = new Request(video, endPoint, number);
+                requests.add(request);
+            }
+
+            return new Problem(videos, caches, requests, endPoints);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static class Problem {
+        public final ArrayList<Video> videos;
+        public final ArrayList<Cache> caches;
+        public final ArrayList<Request> requests;
+        public final ArrayList<EndPoint> endPoints;
+
+        public Problem(ArrayList<Video> videos,
+                       ArrayList<Cache> caches,
+                       ArrayList<Request> requests,
+                       ArrayList<EndPoint> endPoints) {
+            this.videos = videos;
+            this.caches = caches;
+            this.requests = requests;
+            this.endPoints = endPoints;
+        }
+    }
+
 
     public static class Video {
         public final int id;
@@ -56,10 +156,12 @@ public class Qualification {
     }
 
     public static class EndPoint {
+        public final int id;
         public final int latencyFromDataCenter;
         public final ArrayList<Latency> latencies;
 
-        public EndPoint(int latencyFromDataCenter) {
+        public EndPoint(int id, int latencyFromDataCenter) {
+            this.id = id;
             this.latencyFromDataCenter = latencyFromDataCenter;
             this.latencies = new ArrayList<Latency>();
         }
